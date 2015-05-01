d0c0f62c454227a812a90c884063be51c0d0d06a
Djonnni <zavsup222@mail.ru>
2015-05-02 01:50:48 +0600

    Дополнение

--- lab1/views.py
+++ lab1/views.py
@@ -41,34 +41,58 @@ class Person:
         self.name = name
         self.surname = surname
         self.patronymic = patronymic
+# Функция get_fio
+# Данная функция возвращает ФИО студента
 
     def get_fio(self):
         return self.surname + self.name + self.patronymic
     pass
+
+
+# Функция average_subject_value
+# Данная функция находит средний балл по id заданного предмета
+# Рабочим списком данной функции является students_statistics, представляющий собой список из словарей
+def average_subject_value(subject_id):
+    # Переменная average - суммарный балл по дисциплине
+    average = 0.0
+    for student_id in range(len(students_statistics)):
+        #average = students_statistics[<индекс студента>][ключ]
+        average += students_statistics[student_id][subjects_example[subject_id]]
+    return average/len(subjects_example)
+
 # Задание списка из ФИО студентов
 names = ['Danil ', 'Timur ', 'Kunnei ', "Al'bina ", 'Ilona ']
 surnames = ['Zaripov ', 'Koyshinov ', 'Tomskaya ', 'Oun ', 'Sagalakova ']
 patronymics = ["Valer'evich", 'Samatuli', 'Martovna', 'Andreevna', "Vasil'evna"]
+# students_fio - список, содержащий ФИО всех студентов
 students_fio = []
 for i in range(len(names)):
     students_fio.append(Person(names[i], surnames[i], patronymics[i]))
 # Задание списка из преподаваемых предметов
-subjects_example = ['timp', 'EiS', 'philosophy', 'english', 'sport']
+# subject_example - список из интересующих нас дисциплин
+subjects_example = ['timp', 'eis', 'philosophy', 'english', 'sport']
+# subjects - список, содержащий id предметов и их наименования
 subjects = []
-for i in range(len(subjects_example)):
-    subjects.append(Subject(i, subjects_example[i]))
+for subject_id in range(len(subjects_example)):
+    subjects.append(Subject(subject_id, subjects_example[i]))
 # Задание списка с полной информацией о студентах
+# students_other_information - список, содержащий id студентов, их ФИО, группы и возраст
 students_other_information = []
-for i in range(len(names)):
-    students_other_information.append(Student(i, students_fio[i], 743, 19))
+for student_id in range(len(names)):
+    students_other_information.append(Student(student_id, students_fio[i], 743, 19))
 # Задание списка успеваемости студентов
+# scores - список, содержащий все баллы студентов
 scores = []
 for student_id in range(len(names)):
     for subject_id in range(len(subjects)):
         scores.append(Score(student_id, subject_id, random.randint(1, 5)))
-# Задание списка средних баллов студентов
+# Задание списка средних баллов студентов и нахождение среднего значения по средним баллам
+# average_scores - список, содержащий средний балл студентов по дисциплинам
+# average_score - суммарный балл студентов по дисциплине
+# average_value - среднее значение средних баллов по дисциплинам
 average_scores = []
 average_score = 0.0
+average_value = 0.0
 # Так как успеваемость студентов можно представить как двумерный массив, то
 # для вычисления среднего балла по дисциплинам, мы должны пройтись по каждому студенту(по каждой строке)
 # и после перейти на следующую строку
@@ -77,10 +101,14 @@ for student_id in range(len(names)):
     for subject_id in range(len(subjects_example)):
         average_score += scores[student_id*len(subjects_example)+subject_id].value
     average_scores.append(average_score/len(subjects_example))
+    average_value += average_score/len(subjects_example)
     average_score = 0.0
+average_value /= len(names)
 # Зададим список из словарей
+# students_statistics - список из словарей, содержащих id студентов, ФИО, баллы по дисциплинам и средний балл
 students_statistics = []
 for student_id in range(len(names)):
+    # student_info - словарь, содержащий id студента, ФИО, баллы по дисциплинам и средний балл
     student_info = {
         'id': student_id+1,
         'fio': students_fio[student_id].get_fio(),
@@ -92,7 +120,21 @@ for student_id in range(len(names)):
         'average': average_scores[student_id]
     }
     students_statistics.append(student_info)
+# Вычислим средние баллы по всем дисциплинам и добавим их в список
+student_info = {
+    'id': '#',
+    'fio': 'Average Scores',
+    'timp': average_subject_value(0),
+    'eis': average_subject_value(1),
+    'philosophy': average_subject_value(2),
+    'english': average_subject_value(3),
+    'sport': average_subject_value(4),
+    'average': average_value
+}
+students_statistics.append(student_info)
 # Зададим строки из отличников и отчисляемых
+# bad_students - строка, содержащая имена студентов на отчисление
+# excellent_students - строка, содержащая имена студентов - отличников
 bad_students = ''
 excellent_students = ''
 for student_id in range(len(names)):
