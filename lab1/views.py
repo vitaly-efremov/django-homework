e943498b173c5064ee1b4a56a987789c3ae894ae
AlexeyCheremnykh <chmnkh@gmail.com>
2016-04-29 23:39:11 +0400

    29.04.2016

--- lab1/views.py
+++ lab1/views.py
@@ -7,72 +7,63 @@ class IndexView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
-        listOfStudents = ['Боровков Сергей', 'Бусыгин Иван', 'Ворожцов Сергей', 'Герасимов Вячеслав',
+        list_of_students = ['Боровков Сергей', 'Бусыгин Иван', 'Ворожцов Сергей', 'Герасимов Вячеслав',
                           'Ерохин Евгений', 'Иванов Александр', 'Косаченко Татьяна', 'Кравченко Александр',
                           'Кузнецова Мария', 'Лозинг Варвара', 'Мартынов Роман', 'Машуков Никита',
                           'Мухин Алексей', 'Мякишева Эвелина', 'Пехова Анна', 'Сапунов Антон',
                           'Черемных Алексей', 'Шивцов Данил', 'Федурин Артем']
-        listOfStats = []
-        Statistics.ExcStudents = []
-        Statistics.BadStudents = []
-        for i in range(len(listOfStudents)):
-            Student.id = i
-            Student.name = listOfStudents[i]
-            Score.timp = random.randint(2, 5)
-            Score.eis = random.randint(2, 5)
-            Score.eng = random.randint(2, 5)
-            Score.phil = random.randint(2, 5)
-            Score.sport = random.randint(2, 5)
-            Score.average = (Score.timp + Score.eis + Score.eng + Score.phil + Score.sport) / 5
-            if Score.average >= 4.5:
-                Statistics.ExcStudents.append(listOfStudents[i])
-            elif Score.average <= 2.5:
-                Statistics.BadStudents.append(listOfStudents[i])
-            listOfStats.append({'id': Student.id, 'fio': Student.name, 'timp': Score.timp,
-                                'eis': Score.eis, 'philosophy': Score.phil, 'english': Score.eng,
-                                'sport': Score.sport, 'average': Score.average})
+        list_of_bad_students = [] #на отчисление
+        list_of_good_students = [] #отличники
+        list_of_stats = [] #список с оценками, фио, и идами
+        list_of_ids = Student.get_ids(list_of_students)
+        list_of_scores = Score.get_scores(list_of_students)
+        for i in range(len(list_of_students)):
+            average_score = Score.get_average(list_of_scores[i])
+            if average_score >= 4.2:
+                list_of_good_students.append(list_of_students[i])
+            elif average_score <= 2.7:
+                list_of_bad_students.append(list_of_students[i])
+            list_of_stats.append({'id': list_of_ids[i], 'fio': list_of_students[i], 'timp': list_of_scores[i]['timp'],
+                                'eis': list_of_scores[i]['eis'], 'philosophy': list_of_scores[i]['phil'], 
+                                'english': list_of_scores[i]['eng'], 'sport': list_of_scores[i]['sport'],
+                                'average': average_score})
 
         context.update(
             {
-                'students_statistics': listOfStats,
-                #[{'id': n,
-                #                          'fio': Student.name,
-                #                          'timp': random.randint(2,5),
-                #                          'eis': random.randint(2,5),
-                #                          'philosophy': random.randint(2,5),
-                #                          'english': random.randint(2,5),
-                #                          'sport': random.randint(2,5)
-                #                          } for n in range(len(listOfStudents))],
-                # 'students_statistics': [
-                #     {
-                #         #'id': stud1.id,
-                #         'fio' : stud1.name,
-                #         # 'timp': 322,
-                #         # 'eis': 3,
-                #          'philosophy': 4,
-                #         # 'english': 5,
-                #         # 'sport': 2.3,
-                #         # 'average': 2.3,
-                #     },
-                # ],
-                    'excellent_students': Statistics.ExcStudents,
-                    'bad_students': Statistics.BadStudents
+                'students_statistics': list_of_stats,
+                'excellent_students': list_of_good_students,
+                'bad_students': list_of_bad_students
             }
         )
         return context
 
 
-class Student: #id,fio
-    pass
-
-
+class Student: 
+    def get_ids(list_of_students):
+        list_of_ids = [] 
+        for i in range(len(list_of_students)):
+            list_of_ids.append(i)
+        return list_of_ids
+        
 class Statistics: #badStudents, excStudents
-    # student_id, [Subjects]
     pass
 
-class Subject:
-    pass
 
 class Score: #eis, fil, timp, eng, sport
-    # Subject,
-    pass
+    def get_scores(list_of_students):
+        list_of_scores = []
+        sum_of_scores = 0
+        #создается список, в котором каждый элемнет является словарем с оценками каждого студента
+        for i in list_of_students:
+            list_of_scores.append({'timp':random.randint(2, 5), 'eis':random.randint(2, 5), 
+                                   'sport':random.randint(2, 5), 'phil':random.randint(2, 5), 
+                                   'eng':random.randint(2, 5)})
+        return list_of_scores
+    
+    def get_average(scores_of_student):
+        sum_of_scores = 0
+        # сумма оценок, чтобы найти потом среднее
+        for score in scores_of_student.values():
+             sum_of_scores += score
+        average = sum_of_scores / 5  
+        return average   
