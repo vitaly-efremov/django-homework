f2e206c7e3a8e222513a842cd5e96335d9dea45e
grays707 <grays_707@mail.ru>
2016-05-01 00:18:01 +0600

    Update views.py

--- lab1/views.py
+++ lab1/views.py
@@ -10,21 +10,15 @@ class IndexView(TemplateView):
         stdnts = [ 'Первый Первый ' , ' Второй Второй' , ' Третий Третий ' , ' Четвертый Четвертый ' , 
                     ' Пятый Пятый' , ' Шестой Шестой ' , ' Седьмой Седьмой ' , ' Восьмой Восьмой ' ,
                     ' Девятый Девятый ' , ' Десятый Десятый ']
-        bstdnts = [] # bad student
-        gstdnts = [] # Good student(point = 5)
-        point = point.crt_point(stdnts) 
-        P_id = sdnts.crt_id(stdnts)
+        bstdnts = sort.find(stdnts) 
+        gstdnts = sort.find(stdnts) # Good student(point = 5)
+        ss = score.crt_ss(stdnts) # 1st S-subject; 2nd S-score 
+        p_id = ids.crt_id(stdnts)
         stts= [] # stdnts, points, P_ids
-        subj= [ ' smthng1 ' , ' smthng2 ' , 'smthng3' , 'smthng4' , 'smthng5' ]
+        
         
         for i in range(len(stdnts)):
             point=
-        
-        
-        
-        
-        
-        
         
         
         context.update(
@@ -37,7 +31,7 @@ class IndexView(TemplateView):
         return context
 
 
-class Student:
+class ids:
     def crt_id(stdnts)
         P_id=[]
         for i in range(len(stdnts)):
@@ -45,13 +39,41 @@ class Student:
             return P_id
 
 
+class sort: # sorting students on bad and good
+def find(stdnts,ss,p_id)
+for i in range(len(stdnts)):
+   value = statistics.crt_value(stdnts[i])
+   if value >=3.5:
+       gstdnts.appen(stdnts[i])
+    elif value <=3.5:
+        bstdnts.append(stdnts[i])
+stts.append({
+    'student ' : stdnts, 
+    ' id ' : p_id, 
+    'эис': ss[i]['эис']
+    'Физика': ss[i]['Физика']
+    'Програмирование': ss[i]['Програмирование']
+    'МатАн': ss[i]['МатАн']   
+    'Физ-ра': ss[i]['Физ-ра']  
+    'Средний бал': value
+    }) 
+   return stts
+   
+
+class score:
+ def crt_ss(stdnts):
+     ss= []
+     for i in stdnts:
+         ss.append({ ' ЭИС ':random.randint(0, 5) , ' Физика ':random.randint(0, 5) , 
+         'Програмирование':random.randint(0, 5) , 'МатАн':random.randint(0, 5) , 'Физ-ра':random.randint(0, 5) })
+return ss
+
+
 class Statistics:
-    # student_id, [Scores]
-    pass
-
-class Subject:
-    pass
-
-class Score:
-    # Subject, Student, value
-    pass
+   def crt_value(ss):
+       v=0
+       for score in ss.values():
+           V += score 
+           value = v/5
+           return value
+    
