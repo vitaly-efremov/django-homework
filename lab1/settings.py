580f51c85c0643b2d30324718b1e4a7ae059b426
Vladislav Retivykh <firolunis@riseup.net>
2016-05-01 23:49:54 +0600

    Added staticfiles

--- lab1/settings.py
+++ lab1/settings.py
@@ -81,6 +81,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 TEMPLATE_DIRS = (
     os.path.join(os.path.dirname(__file__), 'templates'),
 )
--- lab1/urls.py
+++ lab1/urls.py
@@ -4,8 +4,13 @@ from django.contrib import admin
 
 from .views import IndexView
 
+import os
+
 admin.autodiscover()
 
+site_media = os.path.join(os.path.dirname(__file__), 'static')
+
 urlpatterns = patterns('',
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': site_media}),
     url(r'^$', IndexView.as_view()),
 )
