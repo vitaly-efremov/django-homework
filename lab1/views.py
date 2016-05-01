643b2460d9b25b519b16d9f32083df35266dee18
Vladislav Retivykh <firolunis@riseup.net>
2016-05-02 02:50:38 +0600

    Small fixes

--- lab1/views.py
+++ lab1/views.py
@@ -22,27 +22,27 @@ class IndexView(TemplateView):
 class Statistics:
 
     def __init__(self, subjects, students, scores):
-        self._subjects = subjects
-        self._students = students
-        self._scores = scores
-        self._statistics = []
+        self.__subjects = subjects
+        self.__students = students
+        self.__scores = scores
+        self.__statistics = []
         self.update_statistics()
 
     def update_statistics(self):
-        self._statistics = []
-        for student in self._students.get_students():
+        self.__statistics = []
+        for student in self.__students.get_students():
             current_student = {
                 "id": student.get_id(),
                 "fn": student.get_fn(),
             }
-            scores = self._scores.get_scores_by_student(student)
+            scores = self.__scores.get_scores_by_student(student)
             total_score = sum(score for score in scores.values())
             current_student.update(scores)
             current_student['average'] = total_score/len(scores)
-            self._statistics.append(current_student)
+            self.__statistics.append(current_student)
 
     def get_statistics(self):
-        return self._statistics
+        return self.__statistics
 
     def get_excellent_students(self):
         excellent_students = []
@@ -62,102 +62,102 @@ class Statistics:
 class Students:
 
     def __init__(self):
-        self._students = []
+        self.__students = []
 
     def add(self, fn):
         current_student = Student(fn)
-        self._students.append(current_student)
+        self.__students.append(current_student)
         return current_student
 
     def get_student_by_fn(self, fn):
-        for student in self._students:
+        for student in self.__students:
             if fn == student.get_fn():
                 return student
         return None
 
     def get_students(self):
-        return self._students
+        return self.__students
 
 
 class Student:
-    _inc = 1
+    __inc = 1
 
     def __init__(self, fn):
-        self._id = Student._inc
-        self._fn = fn
-        Student._inc += 1
+        self.__id = Student.__inc
+        self.__fn = fn
+        Student.__inc += 1
 
     def get_id(self):
-        return self._id
+        return self.__id
 
     def get_fn(self):
-        return self._fn
+        return self.__fn
 
 
 class Subjects:
 
     def __init__(self):
-        self._subjects = []
+        self.__subjects = []
 
     def add(self, name):
-        self._subjects.append(Subject(name))
+        self.__subjects.append(Subject(name))
 
     def get_subjects(self):
-        return self._subjects
+        return self.__subjects
 
     def get_subject_by_name(self, name):
-        for subject in self._subjects:
+        for subject in self.__subjects:
             if name == subject.get_name():
                 return subject
         return None
 
 
 class Subject:
-    _inc = 1
+    __inc = 1
 
     def __init__(self, name):
-        self._id = Subject._inc
-        self._name = name
-        Subject._inc += 1
+        self.__id = Subject.__inc
+        self.__name = name
+        Subject.__inc += 1
 
     def get_name(self):
-        return self._name
+        return self.__name
 
 
 class Scores:
 
     def __init__(self):
-        self._scores = []
+        self.__scores = []
 
     def add(self, student, subject, score):
-        self._scores.append(Score(student, subject, score))
+        self.__scores.append(Score(student, subject, score))
 
     def get_scores_by_student(self, student):
         scores = {}
-        for score in self._scores:
+        for score in self.__scores:
             if student.get_fn() == score.get_student().get_fn():
                 scores[score.get_subject().get_name()] = score.get_score()
         return scores
 
 
 class Score:
-    _inc = 1
+    __inc = 1
 
     def __init__(self, student, subject, score):
-        self._id = Score._inc
-        self._student = student
-        self._subject = subject
-        self._score = int(score)
-        Score._inc += 1
+        self.__id = Score.__inc
+        self.__student = student
+        self.__subject = subject
+        self.__score = int(score)
+        Score.__inc += 1
 
     def get_student(self):
-        return self._student
+        return self.__student
 
     def get_subject(self):
-        return self._subject
+        return self.__subject
 
     def get_score(self):
-        return self._score
+        return self.__score
 
 subjects = Subjects()
 students = Students()
