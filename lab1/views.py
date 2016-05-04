87977a2d5948271bd430392793f1d4d0ab3066d7
Asya Kovalchuk <kovalchuk.asya@gmail.com>
2016-05-04 15:40:33 +0600

    It works!

--- lab1/views.py
+++ lab1/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.views.generic.base import TemplateView
-
+from random import random
 
 class IndexView(TemplateView):
     template_name = "index.html"
@@ -9,36 +9,82 @@ class IndexView(TemplateView):
         context = super(IndexView, self).get_context_data(**kwargs)
         context.update(
             {
-                'students_statistics': [
-                    {
-                        'id': 1,
-                        'fio': 'Someone',
-                        'timp': 2,
-                        'eis': 3,
-                        'philosophy': 4,
-                        'english': 5,
-                        'sport': 2.3,
-                        'average': 2.3,
-                    }
-                ],
-                'excellent_students': 'Student A, Student B',
-                'bad_students': 'Student C, Student D'
+                'students_statistics': statistics.get_statistics(),
+                'excellent_students': statistics.get_excellent_students(),
+                'bad_students': statistics.get_bad_students()
             }
         )
         return context
 
 
-class Student:
-    pass
-
-
 class Statistics:
-    # student_id, [Scores]
-    pass
+    def __init__(self, subjects, students, scores):
+        self.statistics = [{'id': student.get_id(), 'fio': student.get_fio()} for student in students]
+        for score in scores:
+            self.statistics[score.get_student().get_id()-1][score.get_subject().get_name()] = score.get_score()
+        for i, student in enumerate(self.statistics):
+            average = 0
+            for subject in subjects:
+                average += student[subject.get_name()]
+            average /= len(subjects)
+            self.statistics[i]['average'] = average
+        self.excellent_students = ', '.join(student['fio'] for student in self.statistics if student['average'] >= 4)
+        self.bad_students = ', '.join(student['fio'] for student in self.statistics if student['average'] < 2.5)
+    def get_statistics(self):
+        return self.statistics
+    def get_excellent_students(self):
+        return self.excellent_students
+    def get_bad_students(self):
+        return self.bad_students
+
+class Student:
+    def __init__(self, id, fio):
+        self.id = id
+        self.fio = fio
+    def get_id(self):
+        return self.id
+    def get_fio(self):
+        return self.fio
 
 class Subject:
-    pass
+    def __init__(self, name):
+        self.name = name
+    def get_name(self):
+        return self.name
 
 class Score:
-    # Subject, Student, value
-    pass
+    def __init__(self, student, subject, score):
+        self.student = student
+        self.subject = subject
+        self.score = score
+    def get_student(self):
+        return self.student
+    def get_subject(self):
+        return self.subject
+    def get_score(self):
+        return self.score
+
+subjects = [
+    Subject('timp'),
+    Subject('eis'),
+    Subject('philosophy'),
+    Subject('english'),
+    Subject('sport')
+]
+
+students = [
+    Student(1 ,'Заммет Тобиас'),
+    Student(2 ,'Гилмор Дэвид'),
+    Student(3 ,'Мальмстин Ингви'),
+    Student(4 ,'Хэтфилд Джеймс'),
+    Student(5 ,'Турунен Тарья'),
+    Student(6 ,'Осборн Оззи'),
+    Student(7 ,'Плант Роберт'),
+    Student(8 ,'Джонсон Брайан'),
+    Student(9 ,'Фрипп Роберт'),
+    Student(10 ,'Роуз Эксл'),
+]
+
+scores = [Score(student, subject, int(random()*5)+1) for subject in subjects for student in students]
+
+statistics = Statistics(subjects, students, scores)
