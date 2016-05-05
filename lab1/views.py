7c2e8f95167a9612f59d5728d2a0d9343be42852
Melkorka95 <radical_cut@inbox.ru>
2016-05-05 14:23:52 +0600

    Update views.py

--- lab1/views.py
+++ lab1/views.py
@@ -4,37 +4,38 @@ from django.views.generic.base import TemplateView
 
 class Student(object):
     def __init__(self, s_num, s_name):
-        self.student = dict(zip(s_num, s_name))
-    def get_s_name(self, s_num):
-        return self.student.setdefault(s_num)
+        self.s_num=s_num
+        self.s_name=s_name
 
-class Subject(object):
-    def __init__(self, subject):
+class Subject:
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+
+class Score:
+    def __init__(self, student, subject, value):
+        self.student = student
         self.subject = subject
-    def get_subject(self):
-        return self.subject
-    
-class Score(object):
-    def __init__(self, rez, s_num):
-        self.student_marks = dict(zip(s_num, rez))
-    def get_rez(self, s_num):
-        return self.student_marks.setdefault(s_num)
-    def bad_student(self):
-        return [i[0] for i in self.student_marks.items() if i[1].count(2) != 0]
-    def excellent_student(self, number_of_subjects):
-        return [i[0] for i in self.student_marks.items() if i[1].count(5) == number_of_subjects]  
+        self.value = value
 
-class Statistics(object):
-    def __init__(self, rez):
-        self.rez = rez
-    def _average_grade(self):
-        return float(sum(self.rez))/len(self.rez)
-    def formatter(self, s_num, s_name, subject):
-        data = {'id':s_num, 'name':s_name}
-        foo = dict(zip(subject, self.rez))
-        data.update(foo)
-        data.update({'average':self._average_grade()})
-        return data
+
+class Statistics:
+    def __init__(self):
+        self.stat = ()
+
+    def student(self, s_num, s_name):
+        self.student = Student(s_num, s_name)
+
+    def score(self, s1, s2, s3, s4, s5):
+        self.s1 = Score(self.student, sub1, s1)                 
+        self.s2 = Score(self.student, sub2, s2)
+        self.s3 = Score(self.student, sub3, s3)
+        self.s4 = Score(self.student, sub4, s4)
+        self.s5 = Score(self.student, sub5, s5)
+
+    def average(self):
+        self.average = float(self.s1.value + self.s2.value + self.s3.value + self.s4.value + self.s5.value)/5
         
         
 class IndexView(TemplateView):
@@ -42,23 +43,38 @@ class IndexView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
-        statistict_stud = []
-        students = Student([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], ['Jarle', 'Ikkeland', 'Kagerou', 'Zukin', 'Koumori', 'Henpuku', 'Noddie', 'Mottleweed', 'Iteza', 'Yatarou'])
-        subject = Subject(['s1', 's2', 's3', 's4', 's5'])
-        score = Score([[5, 4, 4, 5, 5], [2, 3, 4, 2, 4], [3, 3, 4, 3, 5], [3, 5, 5, 2, 5], [3, 5, 2, 4, 5],
-                   [5, 5, 5, 4, 5], [5, 5, 5, 5, 5], [5, 5, 5, 5, 3], [3, 5, 3, 3, 5], [3, 5, 4, 3, 5]],
-                   [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-        for i in range(1,11):
-            statistics = Statistics(score.get_rez(i))
-            statistict_stud.append(statistics.formatter(i, students.get_s_name(i), subject.get_subject()))
-        bad_stud = ', '.join(students.get_s_name(i) for i in score.bad_student())
-        excellent_stud = ', '.join(students.get_s_name(i) for i in score.excellent_student(5))
-        
         context.update(
             {
-                'students_statistics': statistict_stud,
-                'excellent_students': excellent_stud,
-                'bad_students': bad_stud
+                'students_statistics': [
+                    {
+                        'id': s.student.s_num,
+                        'name': s.student.s_name,
+                        's1': s.s1.value,
+                        's2': s.s2.value,
+                        's3':s.s3.value,
+                        's4':s.s4.value,
+                        's5':s.s5.value,
+                        'average': s.average,
+                    }
+                    for s in main
+                ],
+                'excellent_students': excellent_students,
+                'bad_students': bad_students
+
             }
         )
         return context
+sub1 = Subject(1, 's1')
+sub2 = Subject(2, 's2')
+sub3 = Subject(3, 's3')
+sub4 = Subject(4, 's4')
+sub5 = Subject(5, 's5')
+
+
+main = [stud1, stud2, stud3, stud4, stud5, stud6, stud7, stud8, stud9, stud10, stud11]
+for s in main:
+    s.average()
+excellent = [s.student.s_name for s in main if s.average == 5]
+bad = [s.student.s_name for s in main if s.average <= 2.5]
+excellent_students = ', '.join(excellent)
+bad_students = ', '.join(bad)
