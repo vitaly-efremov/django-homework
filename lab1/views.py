49df15de7471b4896233bd2079d425c58edfff8b
Fenrik <nikita-leshev@mail.ru>
2016-05-18 19:29:29 +0600

    2
    Лаба 2

--- lab1/views.py
+++ lab1/views.py
@@ -6,39 +6,67 @@ class IndexView(TemplateView):
     template_name = "index.html"
 
     def get_context_data(self, **kwargs):
+
         context = super(IndexView, self).get_context_data(**kwargs)
+        bad_stud = ', '.join(student.get_name(i) for i in score.bad_student())
+        excellent_stud = ', '.join(student.get_name(i) for i in score.excellent_student(5))
         context.update(
             {
-                'students_statistics': [
-                    {
-                        'id': 1,
-                        'fio': 'Someone',
-                        'timp': 2,
-                        'eis': 3,
-                        'philosophy': 4,
-                        'english': 5,
-                        'sport': 2.3,
-                        'average': 2.3,
-                    }
-                ],
-                'excellent_students': 'Student A, Student B',
-                'bad_students': 'Student C, Student D'
+                        'students_statistics': statis_stud,
+                        'excellent_students': excellent_stud,
+                        'bad_students': bad_stud
+
             }
         )
         return context
 
 
 class Student:
-    pass
+    def __init__(self, studnumber, name):
+        self.student = dict(zip(studnumber, name))
 
+    def get_name(self, studnumber):
+        return self.student.setdefault(studnumber)
 
 class Statistics:
-    # student_id, [Scores]
-    pass
+    def __init__(self, stat):
+        self.stat = stat
+
+    def _average_grade(self):
+        return float(sum(self.stat))/len(self.stat)
+
+    def formatter(self, studnumber, name, subject):
+        file = {'num': studnumber, 'fio': name}
+        timp = dict(zip(subject, self.stat))
+        file.update(timp)
+        file.update({'average':self._average_grade()})
+        return file
 
 class Subject:
-    pass
+    def __init__(self, subject):
+        self.subject = subject
+
+    def get_subject(self):
+        return self.subject
 
 class Score:
-    # Subject, Student, value
-    pass
+    def __init__(self, stat, studnumber):
+        self.stud_mark = dict(zip(studnumber, stat))
+
+    def get_stat(self, studnumber):
+        return self.stud_mark.setdefault(studnumber)
+
+    def bad_student(self):
+        return [i[0] for i in self.stud_mark.items() if i[1].count(2) != 0]
+
+    def excellent_student(self, num_subjects):
+        return [i[0] for i in self.stud_mark.items() if i[1].count(5) == num_subjects]
+
+statis_stud = []
+
+student = Student([1, 2, 3, 4, 5], ['Nikita', 'Andrey', 'Vladimer', 'Max', 'Dmitriy'])
+subject = Subject(['python', 'elektrotex', 'teorInf', 'terVer', 'bos'])
+score = Score([[2, 4, 4, 5, 4], [5, 5, 5, 5, 5], [5, 5, 5, 5, 5], [3, 4, 5, 4, 5], [3, 5, 3, 3, 5]],    [1, 2, 3, 4, 5])
+for i in range(1,6):
+    statis = Statistics(score.get_stat(i))
+    statis_stud.append(statis.formatter(i, student.get_name(i), subject.get_subject()))
