03794c5e9f422c336656ab85a57cc49187733af7
vefremov <efremov.vitaly@gmail.com>
2017-05-22 21:46:21 +0700

    Enhanced to use new Django version 1.11

---
+++ homework/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from django.contrib import admin
+
+from .views import IndexView
+
+urlpatterns = [
+    url(r'^', IndexView.as_view()),
+]
--- homework/wsgi.py
+++ homework/wsgi.py
@@ -1,14 +1,16 @@
 """
-WSGI config for lab1 project.
+WSGI config for homework project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lab1.settings")
 
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "homework.settings")
+
 application = get_wsgi_application()
